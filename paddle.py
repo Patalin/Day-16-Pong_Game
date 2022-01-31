21741d7dd57c868d499deaf4ca8788e3616e793c
Patalin <catalin.palade.92@gmailcom>
2022-01-31 17:22:41 +0000

    Initial commit

---
+++ paddle.py
@@ -0,0 +1,26 @@
+# Created by Patalin.py
+# Follow @Patalin.py on Instagram for more small projects like this
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+
+
+
